fe0f128a57e125b3a9e6985b5c2141c2d5707fc4
dasschnee <jon.schnaecker@gmail.com>
2021-07-17 20:03:30 +0200

    fixed logger in main.py

--- main.py
+++ main.py
@@ -344,4 +344,6 @@ if __name__ == '__main__':
     # print config
     logger.info(config.dump())
 
-    main(config)
+    main(config, logger)
+
+# python -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 main.py --eval --cfg configs/swin_base_patch4_window7_224.yaml --resume swin_base_patch4_window7_224.pth --data-path <imagenet-path>
